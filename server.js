cdad1f7c000c57d0c12c96a08d305c7636c8d73d
Depars <48582531+Depars7@users.noreply.github.com>
2025-12-03 03:33:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000; // Menggunakan variabel PORT dari Koyeb
+const port = process.env.PORT || 3000; // Menggunakan variabel PORT dari Koyeb
 
 app.get('/', (req, res) => {
   res.send('Bot Discord Fish-it is Running!');
